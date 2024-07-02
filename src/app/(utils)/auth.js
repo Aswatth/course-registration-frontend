1297357055eb771830217dea26e5f482d7a1b0fe
Aswatth <aswatth56@gmail.com>
2024-07-02 14:00:46 +0530

    Validating token and user_type before navigating

--- src/app/(utils)/auth.js
+++ src/app/(utils)/auth.js
@@ -1,11 +1,18 @@
 "use server";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
+import { jwtDecode } from "jwt-decode";
 
-export async function checkAuth() {
-  const auth_cookie = cookies().get(process.env.NEXT_PUBLIC_COOKIE_NAME);
+export async function checkAuth(authorized_user) {
+  const auth_token = cookies().get(process.env.NEXT_PUBLIC_COOKIE_NAME);
 
-  if (auth_cookie == undefined) {
+  if (auth_token == undefined) {
     redirect("/login");
+  } else {
+    const decoded_token = jwtDecode(auth_token.value);
+    if (decoded_token.user_type == authorized_user) {
+      redirect("/login");
+    }
   }
+  return true;
 }
