94b95b4a2a11251d4028c5c582a13dab6071b863
Aswatth <aswatth56@gmail.com>
2024-07-10 14:50:10 +0530

    Added styling add-course page

--- src/app/admin/[email_id]/courses/add/page.js
+++ src/app/admin/[email_id]/courses/add/page.js
@@ -3,6 +3,8 @@ import { useState } from "react";
 import * as admin_client from "@/app/(clients)/admin/course_client";
 import { useRouter } from "next/navigation";
 
+import style from "./add_courses.module.css";
+
 export default function AddCourse() {
   const router = useRouter();
   const [new_course, setCourse] = useState({});
@@ -18,70 +20,79 @@ export default function AddCourse() {
   }
 
   return (
-    <div>
-      Add new course
-      <br></br>
-      <label htmlFor="course_id">Course Id:</label>
-      <br></br>
-      <input
-        type="number"
-        id="course_id"
-        name="course_id"
-        required={true}
-        onChange={(e) => {
-          setCourse({ ...new_course, course_id: parseInt(e.target.value) });
-        }}
-      ></input>
-      <br></br>
-      <label htmlFor="course_name">Course name:</label>
-      <br></br>
-      <input
-        type="text"
-        id="course_name"
-        name="course_name"
-        required={true}
-        onChange={(e) => {
-          setCourse({ ...new_course, course_name: e.target.value });
-        }}
-      ></input>
-      <br></br>
-      <label htmlFor="course_description">Course description:</label>
-      <br></br>
-      <input
-        type="text"
-        id="course_description"
-        name="course_description"
-        required={true}
-        onChange={(e) => {
-          setCourse({ ...new_course, course_description: e.target.value });
-        }}
-      ></input>
-      <br></br>
-      <label htmlFor="credits">Credits:</label>
-      <br></br>
-      <input
-        type="number"
-        id="credits"
-        name="credits"
-        required={true}
-        onChange={(e) => {
-          setCourse({ ...new_course, credits: parseInt(e.target.value) });
-        }}
-      ></input>
-      <br></br>
-      <label htmlFor="department">Department:</label>
-      <br></br>
-      <input
-        type="text"
-        id="department"
-        name="department"
-        required={true}
-        onChange={(e) => {
-          setCourse({ ...new_course, department: e.target.value });
-        }}
-      ></input>
-      <br></br>
-      <button onClick={() => createNewCourse()}>Create new course</button>
+    <div className={style["page"]}>
+      <div className={style["header"]}>
+        <h1>Add new course</h1>
+      </div>
+      <div className={style["content"]}>
+        <div className={style["input-decoration"]}>
+          <input
+            type="number"
+            id="course_id"
+            name="course_id"
+            required={true}
+            onChange={(e) => {
+              setCourse({ ...new_course, course_id: parseInt(e.target.value) });
+            }}
+          ></input>
+          <span>Course ID</span>
+        </div>
+        <div className={style["input-decoration"]}>
+          <input
+            type="text"
+            id="course_name"
+            name="course_name"
+            required={true}
+            onChange={(e) => {
+              setCourse({ ...new_course, course_name: e.target.value });
+            }}
+          ></input>
+          <span>Course name</span>
+        </div>
+        <div className={style["input-decoration"]}>
+          <input
+            type="text"
+            id="course_description"
+            name="course_description"
+            required={true}
+            onChange={(e) => {
+              setCourse({ ...new_course, course_description: e.target.value });
+            }}
+          ></input>
+          <span>Course description</span>
+        </div>
+        <div className={style["input-decoration"]}>
+          <input
+            type="number"
+            id="credits"
+            name="credits"
+            required={true}
+            onChange={(e) => {
+              setCourse({ ...new_course, credits: parseInt(e.target.value) });
+            }}
+          ></input>
+          <span>Credits</span>
+        </div>
+        <div className={style["input-decoration"]}>
+          <input
+            type="text"
+            id="department"
+            name="department"
+            required={true}
+            onChange={(e) => {
+              setCourse({ ...new_course, department: e.target.value });
+            }}
+          ></input>
+          <span>Department</span>
+        </div>
+
+        <button
+          className={style["create-course"]}
+          onClick={() => createNewCourse()}
+        >
+          Create new course
+        </button>
+      </div>
     </div>
   );
 }
