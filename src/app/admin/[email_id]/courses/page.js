cf2c82b335fa396121bd53b29f1c9c7d8f851aee
Aswatth <aswatth56@gmail.com>
2024-07-15 12:48:37 +0530

    Updated back button to use icon

--- src/app/admin/[email_id]/courses/page.js
+++ src/app/admin/[email_id]/courses/page.js
@@ -34,7 +34,21 @@ export default function CoursePage() {
   return (
     <div className={style["page"]}>
       <div className={style["header"]}>
-        <button onClick={() => router.back()}>Back</button>
+        <button onClick={() => router.back()}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-chevron-compact-left"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223"
+            />
+          </svg>
+        </button>
         <h1>Courses</h1>
       </div>
       <div className={style["content"]}>
