cfa28e4f5e26c2f5617064342ad13cca1e336b9a
Aswatth <aswatth56@gmail.com>
2024-07-15 14:26:53 +0530

    Added back button to admin-professor pages

--- src/app/admin/[email_id]/professors/add/page.js
+++ src/app/admin/[email_id]/professors/add/page.js
@@ -22,7 +22,24 @@ export default function AddProfessor() {
 
   return (
     <div className={style["page"]}>
-      <div className={style["header"]}>Add professor</div>
+      <div className={style["header"]}>
+        <button onClick={() => router.back()}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-chevron-compact-left"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223"
+            />
+          </svg>
+        </button>
+        <h1>Add professor</h1>
+      </div>
       <div className={style["content"]}>
         <div className={style["input-decoration"]}>
           <input
--- src/app/admin/[email_id]/professors/page.js
+++ src/app/admin/[email_id]/professors/page.js
@@ -34,68 +34,85 @@ export default function ProfessorsPage() {
 
   const professor_content = () => {
     return (
-      <div className={style["page"]}>
-        <div className={style["header"]}>
-          <h1>Professors</h1>
+      <div className={style["content"]}>
+        <div className={style["add"]}>
+          <button
+            className={style["add-button"]}
+            onClick={() => router.push("professors/add")}
+          >
+            Add professor
+          </button>
         </div>
-        <div className={style["content"]}>
-          <div className={style["add"]}>
-            <button
-              className={style["add-button"]}
-              onClick={() => router.push("professors/add")}
-            >
-              Add professor
-            </button>
-          </div>
-          <table>
-            <thead>
-              <tr>
-                <th>First name</th>
-                <th>Last name</th>
-                <th>Email Id</th>
-                <th>Designation</th>
-                <th>Department</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {professor_list.map((m) => {
-                return (
-                  <tr>
-                    <td>{m.first_name}</td>
-                    <td>{m.last_name}</td>
-                    <td>{m.email_id}</td>
-                    <td>{m.designation}</td>
-                    <td>{m.department}</td>
-                    <td>
-                      <div className={style["professor-actions"]}>
-                        <button
-                          className={style["edit-professor"]}
-                          onClick={() =>
-                            router.push("professors/edit/" + m.email_id)
-                          }
-                        >
-                          Edit
-                        </button>
-                        <button
-                          className={style["delete-professor"]}
-                          onClick={() => delete_professor(m.email_id)}
-                        >
-                          DELETE
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>First name</th>
+              <th>Last name</th>
+              <th>Email Id</th>
+              <th>Designation</th>
+              <th>Department</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {professor_list.map((m) => {
+              return (
+                <tr>
+                  <td>{m.first_name}</td>
+                  <td>{m.last_name}</td>
+                  <td>{m.email_id}</td>
+                  <td>{m.designation}</td>
+                  <td>{m.department}</td>
+                  <td>
+                    <div className={style["professor-actions"]}>
+                      <button
+                        className={style["edit-professor"]}
+                        onClick={() =>
+                          router.push("professors/edit/" + m.email_id)
+                        }
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className={style["delete-professor"]}
+                        onClick={() => delete_professor(m.email_id)}
+                      >
+                        DELETE
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     );
   };
 
   return (
-    <div>{professor_list.length == 0 ? no_content() : professor_content()}</div>
+    <div className={style["page"]}>
+      <div className={style["header"]}>
+        <button onClick={() => router.back()}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-chevron-compact-left"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223"
+            />
+          </svg>
+        </button>
+        <h1>Professors</h1>
+      </div>
+      <div>
+        {professor_list.length == 0 ? no_content() : professor_content()}
+      </div>
+    </div>
   );
 }
